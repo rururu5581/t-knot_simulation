d7b77eb200aafc6613da5038d3c152619381ee6a
rururu5581 <sakakurarumiko@gmail.com>
2025-08-25 08:10:22 +0900

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,14 @@
-
 import React from 'react';
 
 const Header: React.FC = () => {
+  // より高品質なロゴ画像のURLに変更しました。
+  const logoUrl = 'https://t-knot.com/wp-content/uploads/2022/01/logo-1.png';
+
   return (
     <header className="bg-white shadow-md">
       <div className="container mx-auto px-6 py-4 flex items-center">
-        <div className="bg-[#00aeef] p-2 rounded-md mr-4">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 8l3 5m0 0l3-5m-3 5v4m0 0H9m3 0h3m-3-5a9 9 0 110-18 9 9 0 010 18z" />
-          </svg>
-        </div>
+        {/* 画像の比率を維持するために object-contain を追加 */}
+        <img src={logoUrl} alt="T.knot Logo" className="h-8 mr-4 object-contain" />
         <h1 className="text-xl font-bold text-gray-800">T.knot 家計診断シミュレーター</h1>
       </div>
     </header>
